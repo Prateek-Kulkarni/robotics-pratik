9957e0c1bd956f2ba7a2d8131df1f921b4b498c5
Pratik Kulkarni <kulkarnipratikd@gmail.com>
2021-02-11 20:31:23 -0500

    hw_2

--- packages/asimo/src/asimo_1subscriber.py
+++ packages/asimo/src/asimo_1subscriber.py
@@ -6,10 +6,11 @@ from mystery_package.msg import UnitsLabelled
 class Listener:
     def __init__(self):
         rospy.Subscriber("/mystery/output1", Float32, self.callback)
-        rospy.Subscriber("/mystery/output2", UnitsLabelled, self.callback)
+        rospy.Subscriber("/mystery/output2", UnitsLabelled, self.callback1)
     def callback(self, msg):
-        rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg.data)
-        
+        rospy.loginfo("/mystery/output1")
+    def callback1(self, msg):
+        rospy.loginfo("/mystery/output2")
 if __name__=='__main__':
     rospy.init_node('asimo1node', anonymous=True)
     Listener()
