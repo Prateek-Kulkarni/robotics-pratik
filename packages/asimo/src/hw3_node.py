cf6d4146e2c4d12179092b63f584bfbc1b3c6ad0
Pratik Kulkarni <kulkarnipratikd@gmail.com>
2021-02-17 12:34:23 -0500

    hw3

---
+++ packages/asimo/src/hw3_node.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import String
+from std_msgs.msg import Float32
+from mystery_package.msg import UnitsLabelled
+class hw3node:
+    def __init__(self):
+        rospy.Subscriber("/mystery/output2", UnitsLabelled, self.callback)
+        self.pub_units = rospy.Publisher("output3", UnitsLabelled, queue_size=10)
+        self.total=0
+        self.pub_msg = UnitsLabelled()
+        self.pub_msg.units= "feet"
+
+    def callback(self, msg):
+         self.total=msg.value
+         self.pub_msg.value = self.total*3.28084
+         self.pub_units.publish(self.pub_msg)
+
+
+
+if __init__ == '__main__':
+    rospy.init_node('hw3_node')
+    hw3node()
+
+    rospy.spin()
