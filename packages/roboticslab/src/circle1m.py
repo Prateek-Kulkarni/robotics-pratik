b5f55e5ebd3af457fdd9ef6d98feee2677367478
Prateek-Kulkarni <kulkarnipratikd@gmail.com>
2021-03-18 12:15:51 -0400

    progresslab2

---
+++ packages/roboticslab/src/circle1m.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32
+from duckietown_msgs.msg import Twist2DStamped
+
+class Circle:
+    def __init__(self):
+        self.pub = rospy.Publisher("car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
+
+    def go(self,y,x):
+        go_msg=Twist2DStamped()
+        go_msg.v=y
+        go_msg.omega=x
+
+        self.pub.publish(move_msg)
+
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('circle1m')
+        pattern=Circle()
+        rate=rospy.Rate(2)
+        count = 0
+        #for count in range(0,100):#10 second timer
+        while not rospy.is_shutdown():
+            if count<20:
+                pattern.go(0.2,1.2)
+            elif count>30:
+                break
+            else:
+                pattern.go(0,0)
+            count += 1
+            rate.sleep()
+
+    except rospy.ROSInterruptException:
+        pass
